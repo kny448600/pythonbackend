17684a14cfb478827a77c85b5083bd0cdd45fae4
kny448600 <kny448600@gmail.com>
2024-06-21 15:25:55 +0900

    SNS

--- main/app.py
+++ main/app.py
@@ -1,8 +1,26 @@
+from flask import flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return"<h1>root directory</h1>"
+
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 app.users = {}
 app.posts = []
 app.idCnt = 1
+
+@app.route('/main', methods=['GET'])
+def signUppage():
+    return render_template('singup.html')
+
+@app.route('/sing-up', methods=['GET']
+           
+           )
+@app.route('/sing-up', methods=['GET'])
 
 @app.route('/sign-up', methods=['POST'])
 def signUp():
@@ -15,6 +33,8 @@ def signUp():
 @app.route('/main', methods=['GET'])
 def main():
     return render_template('main.html', name="seokjin")
+
+
 
 if __name__ == '__name__':
     app.run()
